7cf9bff12055017cb8681ac2bfed9ae9889cbb68
David Ogbeide <david.ogbeide@auticon.ca>
2020-10-15 18:53:05 -0400

    implement EMPTY, SHOW, and basic CREATE appointment states

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,18 +3,37 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
-          interviewer={props.interview.interviewer}
           student={props.interview.student}
-        /> :
-        <Empty />
-      }
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          name={props.interview ? props.interview.student : ''}
+          interviewer={3}
+          interviewers={[]}
+          onSave={() => console.log('onSave')}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
